20273182cd3be8063b891385d0ab3421e561d383
jvin08 <jvin08@gmail.com>
2023-08-30 21:49:45 -0500

    adding project #14

--- src/Data.js
+++ src/Data.js
@@ -212,6 +212,22 @@ const data =   [
   ],
   date: "08 2023",
 },
+
+{
+  id:14,
+  name: 'passwords generator',
+  category: 'security app',
+  link: "https://two-passwords-generator.netlify.app/",
+  info: "generate two passwords at a time",
+  img:
+    'https://raw.githubusercontent.com/jvin08/portfolio_pages/master/gener-front.png',
+  sources: [
+    ["custom checkboxes","https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_custom_checkbox"],
+    ["create clipboardItem","https://developer.mozilla.org/en-US/docs/Web/API/ClipboardItem"],
+    ["get slider value","https://www.w3schools.com/howto/howto_js_rangeslider.asp"],
+  ],
+  date: "08 2023",
+},
 ];
 
 
